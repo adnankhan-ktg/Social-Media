b068572ce932edc73197a73097f82257345a46bb
DESKTOP-44HV636\Admin <aka034437@gmail.com>
2023-10-06 16:05:17 +0530

    Flyway config has been implemented.

--- src/main/java/com/app/model/entity/CategoryMst.java
+++ src/main/java/com/app/model/entity/CategoryMst.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "CATEGORY_MST")
+@Table(name = "category_mst")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
---
+++ src/main/resources/db/migration/V1__insert_category_data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE category_mst(ID INT AUTO_INCREMENT PRIMARY KEY,NAME VARCHAR(50));
+INSERT INTO category_mst(ID, name) VALUES (1, 'TRAVEL');
+INSERT INTO category_mst(ID, name) VALUES (2, 'SPORTS');
+INSERT INTO category_mst(ID, name) VALUES (3, 'DANCING');
+INSERT INTO category_mst(ID, name) VALUES (4, 'SINGING');
+INSERT INTO category_mst(ID, name) VALUES (5, 'RELIGIOUS');
+
